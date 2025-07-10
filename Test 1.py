961d1f6116b21c561802b2330a1009372b3d4efc
alirezamotalebikhah <alirezamotalebikhah@gmail.com>
2025-07-10 19:11:32 +0330

    Define objective & PowerBalance & Storage Equation

--- Test 1.py
+++ Test 1.py
@@ -1,11 +1,11 @@
-from msilib import Binary
+
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from pyomo.environ import *
-from shapely.measurement import bounds
+from torch.fx.experimental.migrate_gradual_types.constraint import Constraint
 
 #define model
 model = ConcreteModel(doc="Home energy Management")
@@ -64,9 +64,10 @@ model.gamma=Param(initialize=0.0971,doc="gamma")
 model.teta=Param(initialize=4.0253,doc="teta")
 model.zeta=Param(initialize=1.0923,doc="zeta")
 model.eta=Param(initialize=0.95, doc="Battery efficiency")
+model.P_max_ch = Param(initialize=2 , doc="max power of Charge and Discharge")
 
-model.p_grid = Var(model.T , bounds=(0,100) , within=NonNegativeReals , doc="Power import from grid")
-model.p_sell = Var(model.T , bounds=(0,100) , within=NonNegativeReals , doc="Power sell to grid")
+model.P_grid = Var(model.T , bounds=(0,100) , within=NonNegativeReals , doc="Power import from grid")
+model.P_sell = Var(model.T , bounds=(0,100) , within=NonNegativeReals , doc="Power sell to grid")
 model.P_ch = Var(model.T ,within=NonNegativeReals , doc="power of Charge Battery")
 model.P_dis = Var(model.T , within=NonNegativeReals , doc="power of Discharge Battery")
 model.P_shift_up = Var(model.T , model.A , within=NonNegativeReals,doc="power of Shift Up")
@@ -77,12 +78,52 @@ model.R_dis = Var(model.T , within=NonNegativeReals , doc="Discharge Rate")
 model.N_cycle = Var( within=NonNegativeReals , doc="Cycle Rate")
 model.SOH =Var(model.T , within=NonNegativeReals , doc="State Of Charge")
 model.Q_loss = Var(model.T , within=NonNegativeReals , doc="Battery Capacity Loss")
+model.P_demand = Var(model.T , within=NonNegativeReals , doc="Battery Demand")
 
 model.u_grid = Var(model.T , within=Binary , doc="sequence of  import power from grid")
 model.u_sell = Var(model.T , within=Binary , doc="sequence of export power to grid")
 model.u_ch = Var(model.T , within=Binary , doc="sequence of Charge Battery")
 model.u_dis = Var(model.T , within=Binary , doc="sequence of Discharge Battery")
-model.u_shif_up = Var(model.T , model.A , within=Binary , doc="sequence of Shift Up")
+model.u_shift_up = Var(model.T , model.A , within=Binary , doc="sequence of Shift Up")
 model.u_shift_down = Var(model.T , model.A , within=Binary , doc="sequence of Shift Down")
+
+def objective(model):
+    return sum(model.P_grid[t] - model.P_sell[t] for t in model.T)
+model.Objective = Objective(rule=objective, sense=minimize , doc="Objective function")
+def balancedemand(model , t):
+    demand =model.P_demand[t] + model.P_ch[t] + model.P_sell[t] + sum(model.P_shift_up[t,a] for a in model.A)
+    generation = model.P_grid[t] + model.P_dis[t] + sum(model.P_shift_down[t,a] for a in model.A) +model.P_pv[t]
+    return demand == generation
+model.BalancedSupply = Constraint(model.T , rule=balancedemand , doc="Balanced Supply")
+def maxcharge(model , t):
+    return model.P_ch[t] <= model.P_max_ch * model.u_ch[t]
+model.MaxCharge=Constraint(model.T , rule=maxcharge , doc="Max Power of Charge")
+def maxdischarge(model , t):
+    return model.P_dis[t] <= model.P_max_ch * model.u_dis[t]
+model.MaxDischarge = Constraint(model.T , rule=maxdischarge , doc="Max Power of Discharge")
+def sequencebattery(model , t):
+    return model.u_ch[t] + model.u_dis[t] <= 1
+model.SequenceBatery= Constraint(model.T , rule=sequencebattery , doc="Battery Sequence")
+def batteryenergymax(model,t):
+    return model.E[t] <= model.BessCapacity
+model.BatteryEnergyMax = Constraint(model.T , rule=batteryenergymax ,doc="Battery Energy Max")
+def ratecharge(model , t):
+    return model.R_ch[t] == (model.eta * model.P_ch[t])/model.BessCapacity
+model.RateCharge = Constraint(model.T , rule=ratecharge , doc="Charge Rate")
+def ratedischarge(model , t):
+    return model.R_dis[t] == (model.P_dis[t])/(model.eta * model.BessCapacity)
+model.RateDischarge=Constraint(model.T , rule=ratedischarge , doc="Discharge Rate")
+def cycleequation(model):
+    return model.N_cycle == sum(model.R_dis[t] for t in model.T)
+model.CycleEq=Constraint(rule=cycleequation , doc="Cycle Equation")
+def cyleconstraints(model):
+    return model.N_cycle <= 0.5
+model.CycleCons=Constraint(rule=cyleconstraints , doc="Cycle Constraints")
+def energyequation(model , t):
+    if t==1 :
+        return model.E[t] == 2
+    else:
+        return model.E[t] == model.E[t-1] + model.eta * model.P_ch[t] - (model.P_dis[t]/model.eta)
+model.EnergyBatteryEq=Constraint(model.T , rule=energyequation , doc="Energy Battery Equation")
 
 
